5cd2ec2c6a77ab0bc4250a03918b59a8a763979d
foobarsik <olgaraat@gmail.com>
2022-08-31 23:22:54 +0200

    fix: fix to GTM

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,13 +9,13 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <Script id="google-tag-manager" strategy="afterInteractive">
-                        {` (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-N7SL9B4');`}
-                    </Script>
+                        })(window,document,'script','dataLayer','GTM-N7SL9B4');`
+                    }}></Script>
                     <link
                         rel="shortcut icon"
                         href="/favicon/favicon.ico"
